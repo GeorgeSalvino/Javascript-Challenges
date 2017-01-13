c354861db25171b8818b38d15be1c5779ef338b3
Shiverzz <georgesalvino98@gmail.com>
2017-01-13 21:06:20 -0200

    seekAndDestroy
    Not finished

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -1,7 +1,25 @@
-
 function destroyer(arr) {
-    
-    return arr;
+	var arrToPush = [];
+	var args = Array.prototype.slice.call(arguments);
+	var numbersToExclude = args.slice(1, args.length);
+	var filtered = numbersToExclude.forEach(function(number){
+		console.log(arrToPush)
+		var filtered = args[0].filter(function DESTROY(searching){
+			console.log(searching + " " + number)
+			if(searching==number){
+				console.log("true")
+				return true
+			}
+			else{
+				console.log("false")
+				return false
+			}
+		})
+		arrToPush.push(filtered)
+		console.log(arrToPush)	
+	})
+	console.log(arrToPush)
+	return filtered
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
